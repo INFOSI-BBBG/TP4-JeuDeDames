7b830133224c03b54a90125fc2d987396a022c59
Omykronn <omykronn.player@gmail.com>
2024-12-20 12:10:06 +0100

    Automatisation du .jar, de la JavaDoc et du rapport de tests
    Par Simon

--- JeuDeDames/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"main.java.org.centrale.medev.jeudedames"},{"l":"test.java.org.centrale.medev.jeudedames"}];updateSearchResults();
--- JeuDeDames/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.centrale.medev.jeudedames","l":"JeuDeDames"},{"p":"org.centrale.medev.jeudedames","l":"Partie"},{"p":"org.centrale.medev.jeudedames","l":"PartieTest"},{"p":"org.centrale.medev.jeudedames","l":"Pion"}];updateSearchResults();
